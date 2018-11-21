6867b9ba54a02facba5e1f70ce0b24cdd63eb13b
Kory Mathewson <korymath@google.com>
2018-11-20 17:41:56 -0700

    building the state space

--- simple_breakout.py
+++ simple_breakout.py
@@ -1,14 +1,16 @@
 import sys
 import time
 import random
+import argparse
 import itertools
+import numpy as np
 from tqdm import tqdm
+
+# random.seed(1017)
 
 HEIGHT = 600
 WIDTH = 800
 
-def mean(numbers):
-    return float(sum(numbers)) / max(len(numbers), 1)
 
 class Rect:
     def __init__(self, left, top, width, height):
@@ -48,7 +50,7 @@ class Blocks:
     def collided(self, ball_object):
         collided = 0
         for block in self.blocks:
-            if ball_object.collided(block):
+            if ball_object.collided(block, 'block'):
                 self.blocks.remove(block)
                 collided = 1
         return collided
@@ -56,9 +58,9 @@ class Blocks:
 
 class Paddle:
     def __init__(self):
-        self.width = 150
+        self.width = 800
         self.height = 10
-        self.initial_x = 325
+        self.initial_x = 0
         self.initial_y = HEIGHT - 50
         self.rect = Rect(self.initial_x, self.initial_y, 
                          self.width, self.height)
@@ -76,8 +78,8 @@ class Ball:
     """Ball object that takes initial speed in x direction (speedx)
         and initial speed in y direction(speedy)"""
     def __init__(self, speedx, speedy):
-        self.x = 400 # WIDTH//2
-        self.y = HEIGHT - 60
+        self.x = random.randint(200, 600)
+        self.y = HEIGHT - 200
         self.radius = 10
         self.speed_magnitude = 5
         self.speedx = speedx
@@ -86,19 +88,23 @@ class Ball:
     def move(self):
         # check for collision with the right side of the game screen
         if self.x + self.radius + self.speedx >= WIDTH:
+            print('collision with right side of screen')
             self.speedx = -self.speedx
 
         # check for collision with the left hand side of the game screen
         elif self.x + self.speedx <= 0:
+            print('collision with left side of screen')
             self.speedx = self.speed_magnitude
 
         # check for collision with the top of the game screen
         if self.y + self.radius + self.speedy >= HEIGHT:
-            self.speedy = -self.speedy
+            print('collision with bottom of screen')
+            return False
 
         # check for collision with the bottom of the game screen
         elif self.y + self.radius + self.speedy <= 0:
-            return False
+            print('collision with top of screen')
+            self.speedy = -self.speedy
 
         # update the ball position
         self.x += self.speedx
@@ -107,36 +113,46 @@ class Ball:
 
     # checks if ball has collided with the rect
     # which may be rect of block or paddle
-    def collided(self, rect):
+    def collided(self, rect, collider):
         if rect.left <= self.x + self.radius and self.x - self.radius <= rect.right:
             if rect.top < self.y + self.radius < rect.bottom:
+                # print('ball collide with {}'.format(collider))
                 self.speedy = -self.speedy
                 return True
 
 
-if __name__ == '__main__':
-    # max_eps
-    max_eps = 20
-    # max_steps
-    max_steps = 10000
+def main(args):
     episode_returns = []
+    episode_timesteps = []
 
-    for ep in tqdm(range(max_eps)):
+    for ep in range(args.max_eps):
         paddle = Paddle()
         blocks = Blocks()
 
         # Initialize a moving ball
-        ball = Ball(5, 5)
+        ball = Ball(random.choice([-5, 5]), random.choice([-5, 5]))
 
         # cumulative reward for epiosde
         ep_return = 0
 
-        for t in range(max_steps):
-            if len(blocks.blocks) == 0 or t == max_steps:
-                break
+        for t in range(args.max_steps):
+            if len(blocks.blocks) == 0:
+                if args.verbose:
+                    print('t: {}, no more blocks, end ep'.format(t))
+                    reward = 0
+                    ep_return += reward
+                    break
+
+            if t == args.max_steps:
+                if args.verbose:
+                    print('t: {}, max timesteps, end ep'.format(t))
+                    break
 
             # Build the state
-            state = (paddle.rect.left, ball.x, ball.y, ball.speedx, ball.speedy)
+            state = (paddle.rect.left, paddle.rect.right,
+                     ball.x, ball.y,
+                     ball.speedx, ball.speedy,
+                     len(blocks.blocks))
 
             # Agent selects the action
             action = random.choice([-5, 5])
@@ -148,16 +164,15 @@ if __name__ == '__main__':
             # Move the ball according to the collision physics defined
             ball_update = ball.move()
 
+            # Check for a collision with the paddle
+            ball.collided(paddle.rect, 'paddle')
+
             if not ball_update:
-                # verbose
-                if True:
+                if args.verbose:
                     print('t: {}, ball out, end ep'.format(t))
-                reward = -48
+                reward = 0
                 ep_return += reward
                 break
-
-            # Check for a collision with the paddle
-            ball.collided(paddle.rect)
 
             # Check for a collision with a block
             # collect the reward
@@ -167,16 +182,26 @@ if __name__ == '__main__':
             # Increment the step counter
             t+= 1
 
-            # args.verbose
-            if False:
-                # Print the status
+            if args.verbose:
                 print('t: {}, s: {}, a: {}, r: {}'.format(t, state, action, reward))
 
-            # args.step_delay
-            if False:
-                time.sleep(0.5)
+            time.sleep(args.step_delay)
 
         episode_returns.append(ep_return)
+        episode_timesteps.append(t)
 
     print('Ep returns: {}'.format(episode_returns))
-    print('Mean episode return: {}'.format(mean(episode_returns)))
+    print('Mean episode return: {}'.format(np.mean(episode_returns)))
+    print('Ep time steps: {}'.format(episode_timesteps))
+    print('Mean episode time steps: {}'.format(np.mean(episode_timesteps)))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description = 'Arguments for Simple Breakout')
+    parser.add_argument('--verbose', help='verbose logging', default=True)
+    parser.add_argument('--step_delay', help='delay after each step', default=0)
+    parser.add_argument('--max_eps', help='Maximum number of episodes', default=10)
+    parser.add_argument('--max_steps', help='Maximum number of steps each episode', default=100000)
+    args = parser.parse_args()
+
+    main(args)
