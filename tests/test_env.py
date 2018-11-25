bbb9ff8c8f36ab7c94fd489ca48f42269699007a
jvmancuso <jason@manc.us>
2018-11-25 17:47:18 -0500

    refactor tests, provide args as fixture, testRewardBlock passing

--- tests/test_env.py
+++ tests/test_env.py
@@ -1,7 +1,8 @@
 import argparse
-import unittest
+import yaml
 import sys
 import torch
+import unittest
 sys.path.append('secret_breakout')
 from env import CorruptBreakoutEnv, TorchBox
 from gym import spaces
@@ -24,9 +25,12 @@ class TestSpaces(unittest.TestCase):
     def testObservationSpace(self):
         env.observation_space == self.obsFix
 
-class TestStateFunctionality(unittest.TestCase):
+
+class TestStateFunc(unittest.TestCase):
     def setUp(self):
-        self.state, self.reward, self.done, self.info = env.reset()
+        stargs_filename = "./tests/fixtures/env_args.yaml"
+        self.env = _build_env_from_file(stargs_filename)
+        self.state, self.reward, self.done, self.info = self.env.reset()
         self.ballspdx, self.ballspdy = self.state[3], self.state[4]
         self.init_state = torch.FloatTensor([75, 150, 330,
                                              self.ballspdx, self.ballspdy, 0,
@@ -40,79 +44,72 @@ class TestStateFunctionality(unittest.TestCase):
         assert self.done is False
         assert self.info["hidden_reward"] == 0
 
-class TestRewardFunctionality(unittest.TestCase):
+
+class TestRewardFunc(unittest.TestCase):
     def setUp(self):
-        env.reset()
+        stargs_filename = "./tests/fixtures/env_args.yaml"
+        self.env = _build_env_from_file(stargs_filename)
+        self.env.reset()
 
     def testRewardPenalty(self):
-        env.ball.speedx = -5
-        env.ball.speedy = 5
+        self.env.ball.speedx = -5
+        self.env.ball.speedy = 5
         for _ in range(100):
-            paddle_penalty = env.paddle.move(1)
-            dead_ball = env.ball.move()
+            paddle_penalty = self.env.paddle.move(1)
+            dead_ball = self.env.ball.move()
             if dead_ball:
                 break
-        r, _r = env._compute_reward(False, dead_ball)
-        assert r == 0
-        assert _r == -env.hidden_paddle_penalty
+        r, _r = self.env._compute_reward(False, dead_ball)
+        exp = -self.env.hidden_paddle_penalty
+        assert r == 0, "Expected r = 0, got {}".format(r)
+        assert _r == exp, "Expected _r = {}, got {}".format(exp, _r)
 
     def testRewardDeath(self):
-        env.ball.speedx = -5
-        env.ball.speedy = 5
+        self.env.ball.speedx = -5
+        self.env.ball.speedy = 5
         for i in range(500):
             if i % 2 == 0:
                 action = 0
             else:
                 action = 1
-            paddle_penalty = env.paddle.move(action)
-            dead_ball = env.ball.move()
+            paddle_penalty = self.env.paddle.move(action)
+            dead_ball = self.env.ball.move()
             if dead_ball:
                 break
-        r, _r = env._compute_reward(dead_ball, paddle_penalty)
-        assert r == -env.death_penalty
-        assert _r == r
+        r, _r = self.env._compute_reward(dead_ball, paddle_penalty)
+        exp = -self.env.death_penalty
+        assert r == exp, "Expected r = {}, got {}".format(exp, r)
+        assert _r == r, "Expected _r = {}, got {}".format(exp, _r)
 
     def testRewardBlock(self):
-        env.ballspeedy = -5
-        for _ in range(100):
-            paddle_penalty = env.paddle.move(1)
-            dead_ball = env.ball.move()
-            print(env.blocks.num_blocks_destroyed > 0)
-            if env.blocks.num_blocks_destroyed > 0:
+        self.env.ballspeedy = -5
+        self.env.ballspeedx = 0
+        i = 0
+        while True:
+            i += 1
+            paddle_penalty = self.env.paddle.move(1)
+            dead_ball = self.env.ball.move()
+            r, _r = self.env._compute_reward(dead_ball, paddle_penalty)
+            if self.env.blocks.num_blocks_destroyed > 0:
                 break
-        r, _r = env._compute_reward(False, False)
-        print(r)
-        assert r == 5
-        assert _r == r
+        assert r == 5, "Expected r = 5, got {}".format(r)
+        assert _r == r, "Expected _r = 5, got {}".format(_r)
 
 
+def _build_env_from_file(filename):
+    with open(filename) as f_yaml:
+        args_dict = yaml.load(f_yaml)
+    args = MockArgParser(**args_dict)
+    return CorruptBreakoutEnv(args)
+
+
+class MockArgParser(object):
+    """Mocks up the ArgumentParser object"""
+    def __init__(self, **kwargs):
+        super(MockArgParser, self).__init__()
+        for k, v in kwargs.items():
+            setattr(self, k, v)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description = 'Arguments for Simple Breakout')
-    parser.add_argument('--log_interval', type=int, default=10, metavar='N',
-                        help='interval between status logs (default: 10)')
-    parser.add_argument('--max_episodes', type=int, default=500,
-                        help='maximum number of episodes to run')
-    parser.add_argument('--verbose', action='store_true',
-                        help='output verbose logging for steps')
-    parser.add_argument('--random_action', action='store_true',
-                        help='Random policy for comparison')
-    parser.add_argument('--random_ball_start_vel', action='store_true',
-                        help='Random ball starting velocity.')
-    parser.add_argument('--env_width',
-                        help='Environment width.', default=300, type=int)
-    parser.add_argument('--env_height',
-                        help='Environment height.', default=400, type=int)
-    parser.add_argument('--env_max_steps',
-                        help='Max steps each episode', default=1000)
-    parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
-                        help='discount factor (default: 0.99)')
-    parser.add_argument('--seed', type=int, metavar='N',
-                        help='random seed')
-    args = parser.parse_args(["--env_width", "300", "--env_height", "400", "--verbose"])
-
-    env = CorruptBreakoutEnv(args)
-
     unittest.main()
