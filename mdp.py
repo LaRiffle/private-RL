1e8d5de575c6be71fb7ece9c4e037b7cfe93a057
Kory Mathewson <korymath@google.com>
2018-12-21 14:07:56 -0700

    working mdp

--- mdp.py
+++ mdp.py
@@ -31,7 +31,7 @@ me.add_workers([bob, alice])
 # after a fire the forest is in youngest state
 
 # probability transition matrix
-# shape: A x S x S
+# shape: A x S x S'
 # each actions transition matrix is indexable by action
 # transitions[a] returns the S x S transition matrix
 # Defines the transition probabilities from all states (rows)
@@ -39,14 +39,16 @@ me.add_workers([bob, alice])
 transitions = sy.FloatTensor(
                       [[[0.5, 0.5],
                         [0.8, 0.2]],
-
                        [[0. , 1.  ],
                         [0.1, 0.9]]])
 
 # reward matrix
-# shape: S x A (rows x columns)
-rewards = sy.FloatTensor([[ 5, 10],
-                          [-1,  2]])
+# shape: A x S x S'
+rewards = sy.FloatTensor(
+                      [[[ 5,  5],
+                        [-1, -1]],
+                       [[10, 10],
+                        [ 2,  2]]])
 
 # discount factor
 gamma = 0.96
@@ -59,6 +61,10 @@ print('rewards shape: {}'.format(rewards.shape))
 # Tensors now live on the remote workers
 # transitions.fix_precision().share(bob, alice)
 # rewards.fix_precision().share(bob, alice)
+
+
+def argmax(iterable):
+    return max(enumerate(iterable), key=lambda x: x[1])[0]
 
 # Value Iteration
 def value_iteration(transitions, rewards, gamma, max_iter=1000, theta=0.01):
@@ -81,88 +87,59 @@ def value_iteration(transitions, rewards, gamma, max_iter=1000, theta=0.01):
 
     # Initialize a value function to hold the long-term value of state s
     values = sy.zeros(num_states)
-    print('Initial V(s): {}'.format(values))
 
     iteration = 0
-    while True:
-        if iteration >= max_iter:
-            break
+    print('t: {}, delta: {}, V(s): {}'.format(
+            iteration, None, list(values)))
 
-        print('Iteration: {}'.format(iteration))
-        print('V(s): {}'.format(values))
+    while True:
+        if iteration > max_iter:
+            break
         iteration += 1
+
         # Stopping condition
         delta = 0
 
-        # BELLMAN OPERATION ON THE VALUE FUNCTION
-        # action_values = sy.zeros(num_actions, num_states)
-        # for a in range(num_actions):
-        #     action_values[a] = rewards[] + gamma * transitions[a].dot(values)
-        # print('bellman Q {}'.format(action_values))
-
-        old_values = values.clone()
-
         # Update each state
         for s in range(num_states):
-            # Find the best action
-            action_values = sy.zeros(num_actions, 1)
-            Q = [rewards[a][s] + gamma * transitions[a][s, :].dot(values) for a in range(num_actions)]
-            print('Q: {}'.format(Q))
+            # store the old state value
+            old_value = values[s]
+
+            # Calulate the action values
+            Q = [sum(transitions[a][s, :] * (rewards[a][s, :] + gamma * values[:])) for a in range(num_actions)]
             values[s] = max(Q)
 
-            # for a in range(num_actions):
-            #     action_value = 0
-            #     for s_next in range(num_states):
-            #         action_value += transitions[a, s, s_next] * (rewards[s_next, a] + gamma * values[s_next])
-            #     action_values[a] = action_value
-            # print('s: {}, action values: {}'.format(s, action_values))
-            # best_action_value = action_values.max()
-
             # Calculate the delta across all seen states
-            delta = max(delta, (values.max() - old_values.min()))
-            # update the value function
-            # values[s] = best_action_value
-        print('t: {}, delta: {}, ep: {}, gamma: {}'.format(iteration, delta, theta, gamma))
+            delta = max(delta, abs(old_value - values[s]))
+
+        # Print stats
+        print('t: {}, delta: {}, ep: {}, gamma: {}, V(s): {}'.format(
+            iteration, delta, theta, gamma, values.cpu().numpy()))
+
         # Check if we can stop
         if delta < (theta * (1 - gamma) / gamma):
              break
 
-    print('\n************************')
-    print('BUILD DETERMINISTIC POLICY')
     # Create a deterministic policy using the optimal value function
     # A policy is a distribution over actions given states
     # Fully defines behaviour of an agent and is stationary
-    policy = []
+    policy = sy.zeros(num_states)
+
+    print('\n************************')
+    print('BUILD DETERMINISTIC POLICY')
     for s in range(num_states):
-        # Find the best action
-        Q = sy.zeros(num_states, 1)
-        for a in range(num_actions):
-            Q[a] = (rewards[a][s] + gamma * transitions[a][s, :].dot(values))
-
-        values[s] = Q.max()
-        policy.append(Q.argmax())
-
-        # action_values = sy.zeros(num_actions, 1)
-        # for a in range(num_actions):
-        #     action_value = 0
-        #     for s_next in range(num_states):
-        #         action_value += transitions[a, s, s_next] * (rewards[s_next, a] + gamma * values[s_next])
-        #     action_values[a] = action_value
-        # # print('action values: {}'.format(action_values))
-        # # Argmax to get the maximizing action
-        # best_action = action_values.max(0)[1]
-        # # Always take the best action
-        # policy[s] = best_action
+        # Calulate the action values
+        Q = [sum(transitions[a][s, :] * (rewards[a][s, :] + gamma * values[:])) for a in range(num_actions)]
+        values[s] = max(Q)
+        policy[s] = argmax(Q)
 
     return values, policy
 
 
 print('\n************************')
-values, policy = value_iteration(transitions, rewards, gamma, max_iter=10)
-print('Optimized value function:')
-print('Values: {}'.format(values))
-print('Optimized policy:')
-print('Policy: {}'.format(policy))
+values, policy = value_iteration(transitions, rewards, gamma, max_iter=100)
+print('Optimized Values: {}'.format(values))
+print('Optimized Policy: {}'.format(policy))
 
 
-# Must make sure that it is a valid MDP
+# TODO(korymath): must make sure that it is a valid MDP
