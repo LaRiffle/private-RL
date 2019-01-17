12caa70192383a25b45fba58fab0b2b6d58310e1
Theo <theo.leffyr@gmail.com>
2019-01-17 21:00:08 +0100

    Fix mdp ex to use and support encrypted data <numerically not working atm>

--- mdp.py
+++ mdp.py
@@ -1,9 +1,14 @@
 import argparse
 import numpy as np
 import syft as sy
-from syft import Variable as Var
-from syft import nn
-from syft import optim
+from syft.core.frameworks.torch.utils import find_tail_of_chain as tail
+
+# PySyft hook
+hook = sy.TorchHook()
+me = hook.local_worker
+bob = sy.VirtualWorker(id="bob", hook=hook)
+alice = sy.VirtualWorker(id="alice", hook=hook)
+me.add_workers([bob, alice])
 
 
 # Value Iteration
@@ -15,44 +20,61 @@ def value_iteration(values, policy, transitions, rewards, gamma, max_iter, theta
     # Bellman Optimality Equation is non-linear
     # No closed form solution, need an iterative solution: Value iteration
 
-    # check theta stopping condition
-    if theta is not None:
-        theta = float(theta)
-        assert theta > 0, "Theta must be greater than 0."
-
     iteration = 0
     num_actions = rewards.shape[0]
     num_states = rewards.shape[1]
     d_state = (int(np.sqrt(num_states)), int(np.sqrt(num_states)))
     print('t: {}, delta: {}, V(s):\n {}'.format(
-        iteration, None, np.reshape(list(values.cpu().numpy()), d_state)))
+        iteration, None, None  # np.reshape(list(values.cpu().numpy()), d_state)
+    ))
 
     while True:
+        print('**************')
         if iteration > max_iter:
             break
         iteration += 1
 
         # Stopping condition
-        delta = 0
+        delta = None
 
         # Update each state
         for s in range(num_states):
+            print('state', s)
             # store the old state value
             old_values = values.clone()
-
             # Sum over next states, and max over actions
-            values[s] = (transitions * (rewards + (gamma * values))).sum(2)[:, s].max()
+            discounted_values = gamma * values.repeat(num_actions, num_states, 1)
+            step_return = public_private_add(rewards, discounted_values)
+            proba_return = public_private_mul(transitions, step_return)
+            expected_value = proba_return.sum(2)[:, s].unsqueeze(0)
+            # cleaning
+            expected_value = expected_value.get().decode().fix_precision().share(alice, bob)
+            new_value_s = expected_value.max()[0]
+            values = private_set(values, s, new_value_s)
+            # values[s] = (transitions * (rewards + (gamma * values))).sum(2)[:, s].max()
 
             # Calculate the delta across all seen states
-            delta = max(delta, abs(old_values[s] - values[s]))
+            delta_step = (old_values[s] - values[s])
+            logic_abs = lambda x: x - 2 * x * ((x * -1) > x)
+            absolute_delta_step = logic_abs(delta_step)
+            logic_max = lambda x, y: x + (y - x)*(y > x)
+            if delta is not None:
+                delta = logic_max(delta, absolute_delta_step)
+            else:
+                delta = delta_step
 
         # Print stats
+        values = values.get().decode()
         print('t: {}, d: {}, g: {}, V:\n {}'.format(
-            iteration, delta,
-            gamma[0], np.reshape(list(values.cpu().numpy()), d_state)))
+            iteration,
+            None,  # delta
+            None,  # gamma[0]
+            np.reshape(list(values.cpu().numpy()), d_state)
+        ))
+        values = values.fix_precision().share(alice, bob)
 
         # Check if we can stop
-        if delta <= theta:
+        if (delta <= theta).get().decode().byte().all():
              break
 
     # Create a deterministic policy using the optimal value function
@@ -61,12 +83,23 @@ def value_iteration(values, policy, transitions, rewards, gamma, max_iter, theta
     print('\n************************')
     print('BUILD DETERMINISTIC POLICY')
     for s in range(num_states):
+        print('step', s)
         # Sum over next states, and max over actions
-        v, i = (transitions * (rewards + (gamma * values))).sum(2)[:, s].max(0)
-        values[s] = v[0]
-        policy[s] = i[0]
+        discounted_values = gamma * values.repeat(num_actions, num_states, 1)
+        step_return = public_private_add(rewards, discounted_values)
+        proba_return = public_private_mul(transitions, step_return)
+        expected_value = proba_return.sum(2)[:, s].unsqueeze(0)
+        # cleaning
+        expected_value = expected_value.get().decode().fix_precision().share(alice, bob)
+        new_value_s = expected_value.max()[0]
+        values = private_set(values, s, new_value_s)
+        # TODO: update policy as well using .max() -> .max(0)
+        # v, i = (transitions * (rewards + (gamma * values))).sum(2)[:, s].max(0)
+        # values[s] = v[0]
+        # policy[s] = i[0]
 
     return values, policy
+
 
 def gridworld():
     """4x4 gridworld example."""
@@ -79,7 +112,7 @@ def gridworld():
     up, down, right, left = range(A)
 
     # Transitions.
-    P = sy.zeros((A, S, S))
+    T = sy.zeros((A, S, S))
 
     # Grid transitions.
     grid_transitions = {
@@ -103,30 +136,21 @@ def gridworld():
     }
     for i, moves in grid_transitions.items():
         for a, j in moves:
-            P[a, i, j] = 1.0
+            T[a, i, j] = 1.0
 
     # Rewards.
-    R = -1 * sy.ones((A, S, S))
+    R = sy.ones((A, S, S)).mul(-1)
     R[:, 0, :] = 0
     R[:, 15, :] = 0
 
-    return P, R
+    return T, R
 
 
 def main(args):
     """Main run function."""
 
-    # PySyft hook
-    hook = sy.TorchHook()
-    me = hook.local_worker
-    bob = sy.VirtualWorker(id="bob", hook=hook)
-    alice = sy.VirtualWorker(id="alice", hook=hook)
-    me.add_workers([bob, alice])
-
     # Build the gridworld
     transitions, rewards = gridworld()
-    # assert that transitions and rewards are shaped the same
-    assert rewards.shape == transitions.shape
 
     # print('transitions: {}'.format(transitions))
     print('transtions shape: {}'.format(transitions.shape))
@@ -146,14 +170,18 @@ def main(args):
     policy = sy.zeros(num_states)
     # Initialize a value function to hold the long-term value of state, s
     values = sy.zeros(num_states)
-    policy.fix_precision().share(bob, alice)
-    values.fix_precision().share(bob, alice)
+    policy = policy.fix_precision().share(bob, alice)
+    values = values.fix_precision().share(bob, alice)
+
+    # Get theta and gamma from args and check value
+    gamma = args.gamma * sy.ones(1)
+    theta = args.theta * sy.ones(1)
+    # check theta stopping condition
+    assert float(theta) > 0, "Theta must be greater than 0."
 
     # Share theta and gamma for learning
-    gamma = args.gamma * sy.ones(1)
-    gamma.fix_precision().share(bob, alice)
-    theta = args.theta * sy.ones(1)
-    theta.fix_precision().share(bob, alice)
+    gamma = gamma.fix_precision().share(bob, alice)
+    theta = theta.fix_precision().share(bob, alice)
 
     # run value iteration
     values, policy = value_iteration(
@@ -163,7 +191,10 @@ def main(args):
         rewards=rewards,
         gamma=gamma,
         theta=theta,
-        max_iter=args.max_iter)
+        max_iter=args.max_iter,
+    )
+    values = values.get().decode()
+    policy = policy.get().decode()
 
     # print results
     print('\n************************')
@@ -174,8 +205,55 @@ def main(args):
     print('Bob objects: {}'.format(bob._objects))
     print('Alice objects: {}'.format(alice._objects))
 
+
+def public_private_add(x_pub, y_priv):
+    x_pub *= 1000  # fixp 10**3
+    if not isinstance(x_pub, sy.LongTensor):
+        x_pub = x_pub.long()
+    gen_pointer = tail(y_priv)
+    assert isinstance(gen_pointer, sy._GeneralizedPointerTensor)
+    for worker_name, pointer in gen_pointer.pointer_tensor_dict.items():
+        worker = pointer.location
+        x_ptr = x_pub.send(worker)
+        y_ptr = pointer.wrap()
+        y_ptr += x_ptr
+        break  # add only one time
+    return y_priv
+
+
+def public_private_mul(x_pub, y_priv):
+    x_pub *= 1000 # fixp 10**3
+    if not isinstance(x_pub, sy.LongTensor):
+        x_pub = x_pub.long()
+    gen_pointer = tail(y_priv)
+    assert isinstance(gen_pointer, sy._GeneralizedPointerTensor)
+    for worker_name, pointer in gen_pointer.pointer_tensor_dict.items():
+        worker = pointer.location
+        x_ptr = x_pub.clone().send(worker)
+        y_ptr = pointer.wrap()
+        y_ptr *= x_ptr
+        y_ptr /= 1000 # rm fixp 10**3
+    return y_priv
+
+
+def private_set(x, idx, v):
+    gen_x = tail(x)
+    gen_v = tail(v)
+    assert isinstance(gen_x, sy._GeneralizedPointerTensor)
+    assert isinstance(gen_v, sy._GeneralizedPointerTensor)
+    assert gen_x.pointer_tensor_dict.keys() == gen_v.pointer_tensor_dict.keys()
+
+    for worker_name, ptr_x in gen_x.pointer_tensor_dict.items():
+        ptr_v = gen_v.pointer_tensor_dict[worker_name].wrap()
+        # worker = ptr_x.location
+        ptr_x = ptr_x.unsqueeze(1)
+        ptr_x[idx, :] = ptr_v
+        ptr_x = ptr_x[:, 0]
+        gen_x.pointer_tensor_dict[worker_name] = ptr_x
+    return x
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description = 'PySyft MDP Gridworld')
+    parser = argparse.ArgumentParser(description='PySyft MDP Gridworld')
     parser.add_argument('--gamma', type=float, default=1.0, help='Discount factor')
     parser.add_argument('--theta', type=float, default=0.0001, help='Learning threshold')
     parser.add_argument('--max_iter', type=int, default=10, help='Maximum number of iterations')
